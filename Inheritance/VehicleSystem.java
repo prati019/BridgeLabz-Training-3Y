6e1a766527cdcb2b3b210167de57e1253f81187f
Pragati Agrawal <pragatia090@gmail.com>
2025-09-20 23:46:08 +0530

    Create VehicleSystem.java

---
+++ Inheritance/VehicleSystem.java
@@ -0,0 +1,60 @@
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType){
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo(){
+        System.out.println("Max Speed: "+maxSpeed+" km/h, Fuel: "+fuelType);
+    }
+}
+
+class Car extends Vehicle {
+    int seatCapacity;
+    Car(int maxSpeed, String fuelType, int seatCapacity){
+        super(maxSpeed,fuelType);
+        this.seatCapacity=seatCapacity;
+    }
+    @Override void displayInfo(){
+        super.displayInfo();
+        System.out.println("Seats: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicle {
+    double loadCapacity;
+    Truck(int maxSpeed,String fuelType,double loadCapacity){
+        super(maxSpeed,fuelType);
+        this.loadCapacity=loadCapacity;
+    }
+    @Override void displayInfo(){
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    boolean hasABS;
+    Motorcycle(int maxSpeed,String fuelType,boolean hasABS){
+        super(maxSpeed,fuelType);
+        this.hasABS=hasABS;
+    }
+    @Override void displayInfo(){
+        super.displayInfo();
+        System.out.println("ABS: " + hasABS);
+    }
+}
+
+public class VehicleSystem {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {
+                new Car(180,"Petrol",5),
+                new Truck(120,"Diesel",10),
+                new Motorcycle(160,"Petrol",true)
+        };
+        for(Vehicle v : vehicles) v.displayInfo();
+    }
+}
