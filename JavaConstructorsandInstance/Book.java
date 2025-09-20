af950a08b3fb613e61c000c96550f443dc366eda
Pragati Agrawal <pragatia090@gmail.com>
2025-09-20 22:37:11 +0530

    Create Book.java

---
+++ JavaConstructorsandInstance/Book.java
@@ -0,0 +1,56 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book() {
+        this.title = "Unknown Title";
+        this.author = "Unknown Author";
+        this.price = 0.0;
+    }
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 9.99);
+
+        System.out.println("Book 1:");
+        System.out.println("Title: " + book1.getTitle());
+        System.out.println("Author: " + book1.getAuthor());
+        System.out.println("Price: $" + book1.getPrice());
+
+        System.out.println("\nBook 2:");
+        System.out.println("Title: " + book2.getTitle());
+        System.out.println("Author: " + book2.getAuthor());
+        System.out.println("Price: $" + book2.getPrice());
+    }
+}
