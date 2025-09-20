7fce9acdca016e8ac37b1160285fcfc8f2d44b99
Pragati Agrawal <pragatia090@gmail.com>
2025-09-20 22:00:04 +0530

    Create SquareSide.java

---
+++ Java-Fundamental/SquareSide.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class SquareSide {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter perimeter of square: ");
+        double perimeter = input.nextDouble();
+
+        double side = perimeter / 4;
+        System.out.println("\nThe side of the square = " + side);
+
+    }
+}
